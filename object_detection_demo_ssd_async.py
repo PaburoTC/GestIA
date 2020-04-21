d11a6f808cf9600073eb6ac031b2e832d3a8d07d
paburo <pablosky301@gmail.com>
2020-04-21 21:59:40 +0200

    Parsers cleaned

--- object_detection_demo_ssd_async.py
+++ object_detection_demo_ssd_async.py
@@ -23,21 +23,14 @@ import cv2
 import time
 import logging as log
 
-import keyboard
 
-from keyboardController import keyboardController
+
 from openvino.inference_engine import IENetwork, IECore
 
 
 def build_argparser():
     parser = ArgumentParser(add_help=False)
     args = parser.add_argument_group('Options')
-    args.add_argument('-h', '--help', action='help', default=SUPPRESS, help='Show this help message and exit.')
-    args.add_argument("-m", "--model", help="Required. Path to an .xml file with a trained model.",
-                      required=True, type=str)
-    args.add_argument("-i", "--input",
-                      help="Required. Path to video file or image. 'cam' for capturing video stream from camera",
-                      required=True, type=str)
     args.add_argument("-l", "--cpu_extension",
                       help="Optional. Required for CPU custom layers. Absolute path to a shared library with the "
                            "kernels implementations.", type=str, default=None)
@@ -45,9 +38,6 @@ def build_argparser():
                       help="Optional. Specify the target device to infer on; CPU, GPU, FPGA, HDDL or MYRIAD is "
                            "acceptable. The demo will look for a suitable plugin for device specified. "
                            "Default value is CPU", default="CPU", type=str)
-    args.add_argument("--labels", help="Optional. Path to labels mapping file", default=None, type=str)
-    args.add_argument("-pt", "--prob_threshold", help="Optional. Probability threshold for detections filtering",
-                      default=0.5, type=float)
     args.add_argument("--no_show", help="Optional. Don't show output", action='store_true')
 
     return parser
@@ -56,7 +46,8 @@ def build_argparser():
 def main():
     log.basicConfig(format="[ %(levelname)s ] %(message)s", level=log.INFO, stream=sys.stdout)
     args = build_argparser().parse_args()
-    model_xml = args.model
+    model_xml = 'gestures_inference_graph_16Abril_10am/ir/fp32/frozen_inference_graph.xml'
+    print(model_xml)
     model_bin = os.path.splitext(model_xml)[0] + ".bin"
 
     log.info("Creating Inference Engine...")
@@ -101,18 +92,11 @@ def main():
     if img_info_input_blob:
         feed_dict[img_info_input_blob] = [h, w, 1]
 
-    if args.input == 'cam':
-        input_stream = 0
-    else:
-        input_stream = args.input
-    cap = cv2.VideoCapture(input_stream)
-    assert cap.isOpened(), "Can't open " + input_stream
+    cap = cv2.VideoCapture(0)
+    assert cap.isOpened(), "Can't open camera 0"
 
-    if args.labels:
-        with open(args.labels, 'r') as f:
-            labels_map = [x.strip() for x in f]
-    else:
-        labels_map = None
+    with open('gesture_labels.txt', 'r') as f:
+        labels_map = [x.strip() for x in f]
 
     cur_request_id = 0
     next_request_id = 1
@@ -126,89 +110,87 @@ def main():
 
     print("To close the application, press 'CTRL+C' here or switch to the output window and press ESC key")
     print("To switch between sync/async modes, press TAB key in the output window")
-    i = 0
     while cap.isOpened():
-        if i%5==0:
-            if is_async_mode:
-                ret, next_frame = cap.read()
-            else:
-                ret, frame = cap.read()
-                if ret:
-                    frame_h, frame_w = frame.shape[:2]
-            if not ret:
-                break  # abandons the last frame in case of async_mode
-            # Main sync point:
-            # in the truly Async mode we start the NEXT infer request, while waiting for the CURRENT to complete
-            # in the regular mode we start the CURRENT request and immediately wait for it's completion
-            inf_start = time.time()
-            if is_async_mode:
-                in_frame = cv2.resize(next_frame, (w, h))
-                in_frame = in_frame.transpose((2, 0, 1))  # Change data layout from HWC to CHW
-                in_frame = in_frame.reshape((n, c, h, w))
-                feed_dict[input_blob] = in_frame
-                exec_net.start_async(request_id=next_request_id, inputs=feed_dict)
-            else:
-                in_frame = cv2.resize(frame, (w, h))
-                in_frame = in_frame.transpose((2, 0, 1))  # Change data layout from HWC to CHW
-                in_frame = in_frame.reshape((n, c, h, w))
-                feed_dict[input_blob] = in_frame
-                exec_net.start_async(request_id=cur_request_id, inputs=feed_dict)
-            if exec_net.requests[cur_request_id].wait(-1) == 0:
-                inf_end = time.time()
-                det_time = inf_end - inf_start
-
-                # Parse detection results of the current request
-                res = exec_net.requests[cur_request_id].outputs[out_blob]
-                #keyboardController(res[0][0])
-                for obj in res[0][0]:
-                    # Draw only objects when probability more than specified threshold
-                    if obj[2] > args.prob_threshold:
-                        keyboard.press_and_release('SPACE')
-                        xmin = int(obj[3] * frame_w)
-                        ymin = int(obj[4] * frame_h)
-                        xmax = int(obj[5] * frame_w)
-                        ymax = int(obj[6] * frame_h)
-                        class_id = int(obj[1])
-
-                        # Draw box and label\class_id
-                        color = (min(class_id * 12.5, 255), min(class_id * 7, 255), min(class_id * 5, 255))
-                        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color, 2)
-                        det_label = labels_map[class_id] if labels_map else str(class_id)
-                        cv2.putText(frame, det_label + ' ' + str(round(obj[2] * 100, 1)) + ' %', (xmin, ymin - 7),
-                                    cv2.FONT_HERSHEY_COMPLEX, 0.6, color, 1)
-
-                # Draw performance stats
-                inf_time_message = "Inference time: N\A for async mode" if is_async_mode else \
-                    "Inference time: {:.3f} ms".format(det_time * 1000)
-                render_time_message = "OpenCV rendering time: {:.3f} ms".format(render_time * 1000)
-                async_mode_message = "Async mode is on. Processing request {}".format(cur_request_id) if is_async_mode else \
-                    "Async mode is off. Processing request {}".format(cur_request_id)
-
-                cv2.putText(frame, inf_time_message, (15, 15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (200, 10, 10), 1)
-                cv2.putText(frame, render_time_message, (15, 30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (10, 10, 200), 1)
-                cv2.putText(frame, async_mode_message, (10, int(frame_h - 20)), cv2.FONT_HERSHEY_COMPLEX, 0.5,
-                            (10, 10, 200), 1)
-
-            #
-            render_start = time.time()
-            if not args.no_show:
-                cv2.imshow("Detection Results", frame)
-            render_end = time.time()
-            render_time = render_end - render_start
-
-            if is_async_mode:
-                cur_request_id, next_request_id = next_request_id, cur_request_id
-                frame = next_frame
+        if is_async_mode:
+            ret, next_frame = cap.read()
+        else:
+            ret, frame = cap.read()
+            if ret:
                 frame_h, frame_w = frame.shape[:2]
+        if not ret:
+            break  # abandons the last frame in case of async_mode
+        # Main sync point:
+        # in the truly Async mode we start the NEXT infer request, while waiting for the CURRENT to complete
+        # in the regular mode we start the CURRENT request and immediately wait for it's completion
+        inf_start = time.time()
+        if is_async_mode:
+            in_frame = cv2.resize(next_frame, (w, h))
+            in_frame = in_frame.transpose((2, 0, 1))  # Change data layout from HWC to CHW
+            in_frame = in_frame.reshape((n, c, h, w))
+            feed_dict[input_blob] = in_frame
+            exec_net.start_async(request_id=next_request_id, inputs=feed_dict)
+        else:
+            in_frame = cv2.resize(frame, (w, h))
+            in_frame = in_frame.transpose((2, 0, 1))  # Change data layout from HWC to CHW
+            in_frame = in_frame.reshape((n, c, h, w))
+            feed_dict[input_blob] = in_frame
+            exec_net.start_async(request_id=cur_request_id, inputs=feed_dict)
+        if exec_net.requests[cur_request_id].wait(-1) == 0:
+            inf_end = time.time()
+            det_time = inf_end - inf_start
 
-            if not args.no_show:
-                key = cv2.waitKey(1)
-                if key == 27:
-                    break
-                if (9 == key):
-                    is_async_mode = not is_async_mode
-                    log.info("Switched to {} mode".format("async" if is_async_mode else "sync"))
-        i+=1
+            # Parse detection results of the current request
+            res = exec_net.requests[cur_request_id].outputs[out_blob]
+            # keyboardController(res[0][0])
+            for obj in res[0][0]:
+                # Draw only objects when probability more than specified threshold
+                if obj[2] > 0.6:
+                    # keyboard.press_and_release('SPACE')
+                    xmin = int(obj[3] * frame_w)
+                    ymin = int(obj[4] * frame_h)
+                    xmax = int(obj[5] * frame_w)
+                    ymax = int(obj[6] * frame_h)
+                    class_id = int(obj[1])
+
+                    # Draw box and label\class_id
+                    color = (min(class_id * 12.5, 255), min(class_id * 7, 255), min(class_id * 5, 255))
+                    cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color, 2)
+                    det_label = labels_map[class_id] if labels_map else str(class_id)
+                    cv2.putText(frame, det_label + ' ' + str(round(obj[2] * 100, 1)) + ' %', (xmin, ymin - 7),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.6, color, 1)
+
+            # Draw performance stats
+            inf_time_message = "Inference time: N\A for async mode" if is_async_mode else \
+                "Inference time: {:.3f} ms".format(det_time * 1000)
+            render_time_message = "OpenCV rendering time: {:.3f} ms".format(render_time * 1000)
+            async_mode_message = "Async mode is on. Processing request {}".format(cur_request_id) if is_async_mode else \
+                "Async mode is off. Processing request {}".format(cur_request_id)
+
+            cv2.putText(frame, inf_time_message, (15, 15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (200, 10, 10), 1)
+            cv2.putText(frame, render_time_message, (15, 30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (10, 10, 200), 1)
+            cv2.putText(frame, async_mode_message, (10, int(frame_h - 20)), cv2.FONT_HERSHEY_COMPLEX, 0.5,
+                        (10, 10, 200), 1)
+
+        #
+        render_start = time.time()
+        if not args.no_show:
+            cv2.imshow("Detection Results", frame)
+        render_end = time.time()
+        render_time = render_end - render_start
+
+        if is_async_mode:
+            cur_request_id, next_request_id = next_request_id, cur_request_id
+            frame = next_frame
+            frame_h, frame_w = frame.shape[:2]
+
+        if not args.no_show:
+            key = cv2.waitKey(1)
+            if key == 27:
+                break
+            if (9 == key):
+                is_async_mode = not is_async_mode
+                log.info("Switched to {} mode".format("async" if is_async_mode else "sync"))
+
     cv2.destroyAllWindows()
 
 
